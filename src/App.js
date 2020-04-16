a99fc2500b3d91d649ed748d55013c641c903aa9
Paulo Rezende <paulo@pmbr.net>
2020-04-16 09:26:02 -0700

    filter and list products

--- src/App.js
+++ src/App.js
@@ -7,19 +7,28 @@ class App extends Component {
   state = {
     products: [
       { description: "t-shirt", price: 1.01 },
-      { description: "shorts", price: 2.99 }
+      { description: "shorts", price: 2.99 },
+      { description: "polo", price: 0 }
     ]
   }
 
   render() {
     return (
       <div className="App">
-         Welcome to Price UI
-         <Product description={this.state.products[0].description} price={this.state.products[0].price} />
-         <Product description={this.state.products[1].description} price={this.state.products[1].price} />
+        Welcome to Price UI
+        {this.state.products
+          .filter(p => p.price > 0)
+          .map(p => 
+            {
+              return <Product description={p.description} price={p.price} />   ;
+            })
+        }
+         
+         
       </div>
     );    
   }
+
 }
 
 export default App;
