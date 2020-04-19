d5de00769a5b4eae75949d16c9d4a80c7afd3753
Paulo Rezende <paulo@pmbr.net>
2020-04-18 19:15:17 -0700

    service post and put

--- price-service.go
+++ price-service.go
@@ -4,103 +4,146 @@ import (
 	"log"
 	"strconv"
 
-	"net/http"
 	"encoding/json"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-type Price struct { 
-	Id                 int      `json:"id"` 
-	ProductId          int      `json:"productId"` 
-	ProductDescription string   `json:"productDescription"` 
-	Price              float32  `json:"price"` 
-	StartDate          string   `json:"startDate"` 
-	EndDate            string   `json:"endDate"` 
-	MaxDiscount        float32  `json:"maxDiscount"` 
+type Price struct {
+	Id                 int     `json:"id"`
+	ProductId          int     `json:"productId"`
+	ProductDescription string  `json:"productDescription"`
+	Price              float32 `json:"price"`
+	StartDate          string  `json:"startDate"`
+	EndDate            string  `json:"endDate"`
+	MaxDiscount        float32 `json:"maxDiscount"`
 }
 
-type Error struct { 
-	ErrorCode	int	`json:"errorCode"` 
-	ErrorMessage string `json:"errorMessage"` 
-	ErrorDetails string `json:"errorDetails"` 
+type Error struct {
+	ErrorCode    int    `json:"errorCode"`
+	ErrorMessage string `json:"errorMessage"`
+	ErrorDetails string `json:"errorDetails"`
 }
 
-var price1 Price = Price {
-				Id: 1,	   
-				ProductId: 10, 
-				ProductDescription: "Blue Jeans", 
-				Price: 1.01, 
-				StartDate: "05/31/2020",
-				EndDate: "06/18/2020",
-				MaxDiscount: 0.5,
-			}
+var price1 Price = Price{
+	Id:                 1,
+	ProductId:          10,
+	ProductDescription: "Blue Jeans",
+	Price:              1.01,
+	StartDate:          "05/31/2020",
+	EndDate:            "06/18/2020",
+	MaxDiscount:        0.5,
+}
 
-var price2 Price = Price {
-				Id: 2,
-				ProductId: 20, 
-				ProductDescription: "Green T-Shirt", 
-				Price: 2.99,
-				StartDate: "01/01/2020",
-				EndDate: "12/31/2020",
-				MaxDiscount: 0.3,
-			} 
+var price2 Price = Price{
+	Id:                 2,
+	ProductId:          20,
+	ProductDescription: "Green T-Shirt",
+	Price:              2.99,
+	StartDate:          "01/01/2020",
+	EndDate:            "12/31/2020",
+	MaxDiscount:        0.3,
+}
 
-var price3 Price = Price {
-				Id: 3,
-				ProductId: 30, 
-				ProductDescription: "White Skirt", 
-				Price: 0, 
-				StartDate: "",
-				EndDate: "",
-				MaxDiscount: 0,
-			} 
+var price3 Price = Price{
+	Id:                 3,
+	ProductId:          30,
+	ProductDescription: "White Skirt",
+	Price:              0,
+	StartDate:          "",
+	EndDate:            "",
+	MaxDiscount:        0,
+}
 
-var price4 Price = Price {
-				Id: 4, 
-				ProductId: 10, 
-				ProductDescription: "Blue Jeans", 
-				Price: 1.67, 
-				StartDate: "06/19/2020",
-				EndDate: "06/30/2020",
-				MaxDiscount: 0,
-			} 
+var price4 Price = Price{
+	Id:                 4,
+	ProductId:          10,
+	ProductDescription: "Blue Jeans",
+	Price:              1.67,
+	StartDate:          "06/19/2020",
+	EndDate:            "06/30/2020",
+	MaxDiscount:        0,
+}
 
-var prices = []Price { price1, price2, price3, price4, }
+var prices = []Price{price1, price2, price3, price4}
 
 func getAll(w http.ResponseWriter, r *http.Request) {
 	addResponseHeaders(w)
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(prices) 
+	json.NewEncoder(w).Encode(prices)
 }
 
 func get(w http.ResponseWriter, r *http.Request) {
 	addResponseHeaders(w)
 	if id, ok := extractIdFromPathParams(w, r); ok {
 		for _, price := range prices {
-			if (price.Id == id) {
+			if price.Id == id {
 				w.WriteHeader(http.StatusOK)
-				json.NewEncoder(w).Encode(price) 
+				json.NewEncoder(w).Encode(price)
 				log.Print("get returned id ", id)
-				return;
+				return
 			}
 		}
 		log.Print("get found no id ", id)
-		w.WriteHeader(http.StatusNotFound)		
-		json.NewEncoder(w).Encode( Error { ErrorMessage: "no price found for id" })
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(Error{ErrorMessage: "no price found for id"})
 	}
 }
 
 func post(w http.ResponseWriter, r *http.Request) {
 	addResponseHeaders(w)
+	var postPrice Price
+	err := json.NewDecoder(r.Body).Decode(&postPrice)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(Error{ErrorMessage: "id payload body"})
+		log.Print("id payload body")
+		return
+	}
+	for _, price := range prices {
+		if price.Id == postPrice.Id {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(Error{ErrorMessage: "id duplicate"})
+			log.Print("id duplicate")
+			return
+		}
+	}
+	prices = append(prices, postPrice)
+	log.Print("created id ", postPrice.Id)
 	w.WriteHeader(http.StatusCreated)
 }
 
 func put(w http.ResponseWriter, r *http.Request) {
 	addResponseHeaders(w)
 	if id, ok := extractIdFromPathParams(w, r); ok {
-		log.Print("updated id ", id)
-		w.WriteHeader(http.StatusOK)
+		var putPrice Price
+		err := json.NewDecoder(r.Body).Decode(&putPrice)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(Error{ErrorMessage: "id payload body"})
+			log.Print("id payload body")
+			return
+		}
+		if id != putPrice.Id {
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(Error{ErrorMessage: "request id and payload id must match"})
+			log.Print("request id and payload id do not match")
+			return
+		}
+		for index, price := range prices {
+			if price.Id == putPrice.Id {
+				prices = append(prices[:index], prices[index+1:]...)
+				prices = append(prices, putPrice)
+				log.Print("updated id ", id)
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(Error{ErrorMessage: "id not found"})
+		log.Print("update found no id")
+
 	}
 }
 
@@ -108,16 +151,16 @@ func delete(w http.ResponseWriter, r *http.Request) {
 	addResponseHeaders(w)
 	if id, ok := extractIdFromPathParams(w, r); ok {
 		for index, price := range prices {
-			if (price.Id == id) {
+			if price.Id == id {
 				prices = append(prices[:index], prices[index+1:]...)
 				w.WriteHeader(http.StatusOK)
 				log.Print("deleted id ", id)
-				return;
+				return
 			}
 		}
 		log.Print("delete found no id ", id)
-		w.WriteHeader(http.StatusNotFound)		
-		json.NewEncoder(w).Encode( Error { ErrorMessage: "no price found for id" })
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(Error{ErrorMessage: "no price found for id"})
 	}
 }
 
@@ -146,13 +189,13 @@ func extractIdFromPathParams(w http.ResponseWriter, r *http.Request) (int, bool)
 		id, err = strconv.Atoi(val)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode( Error { ErrorMessage: "id invalid" })
+			json.NewEncoder(w).Encode(Error{ErrorMessage: "id invalid"})
 			log.Print("received request with invalid id ", val)
 			return -1, false
 		}
 	} else {
 		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode( Error { ErrorMessage: "id required" })
+		json.NewEncoder(w).Encode(Error{ErrorMessage: "id required"})
 		log.Print("received request with id missing")
 		return -1, false
 
@@ -163,5 +206,5 @@ func extractIdFromPathParams(w http.ResponseWriter, r *http.Request) (int, bool)
 func addResponseHeaders(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	w.Header().Set("Access-Control-Allow-Credentials", "true")	
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
 }
